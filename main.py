8feb6f240e7242be9500ab93b00a8a2d7a51a81f
Judan2 <j.mosquerad@uniandes.edu.co>
2026-05-16 22:41:04 -0500

    ultimos retoques
    ya esta todo entero

--- main.py
+++ main.py
@@ -12,34 +12,44 @@ app.add_middleware(
     allow_methods=["*"],
     allow_headers=["*"]
 )
+#----------------------------
+#conexion a la base de datos
 
 #os.environ para despliegue. Descomente cuando ya probó todo local.
 client = MongoClient(os.environ["MONGO_URI"])
 
-db = client["ISIS2304F19202610"] #base de datos 
+db = client["parranderos"] #base de datos 
 
 
 @app.get("/")
 def inicio():
     return {"estado": "API funcionando correctamente"}
 
+
+# Punto 6 - GET comentarios 
 @app.get('/bares/{bar_id}/comentarios')
 def get_comentarios(bar_id: int):
-    comentarios = list(db["comentarios"].find({"bar_id": bar_id}, {"_id": 0}))
-    return comentarios
+    comentarios = list(db["comentarios_bares"].find({"bar_id": bar_id}, {"_id": 0}))
+    return comentarios or {'mensaje': 'No se encontraron comentarios para este bar'}
 
+
+# Punto 7 - POST comentarios (cambiar nombre de colección)
 @app.post('/bares/{bar_id}/comentarios')
 def post_comentario(bar_id: int, datos: dict):
     datos['bar_id'] = bar_id
-    datos['fecha']  = datetime.now().isoformat()
-    db["comentarios"].insert_one(datos)
+    datos['fecha'] = datetime.now().isoformat()
+    db["comentarios_bares"].insert_one(datos)
     return {'mensaje': 'Comentario guardado'}
 
+
+# Punto 8 - GET eventos
 @app.get('/bares/{bar_id}/eventos')
 def get_eventos(bar_id: int):
     eventos = list(db["eventos"].find({"bar_id": bar_id}, {"_id": 0}))
-    return eventos
+    return eventos or {'mensaje': 'No se encontraron eventos para este bar'}
 
+
+# Punto 9 - POST eventos
 @app.post('/bares/{bar_id}/eventos')
 def post_evento(bar_id: int, datos: dict):
     datos['bar_id'] = bar_id
